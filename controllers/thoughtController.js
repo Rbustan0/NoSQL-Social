2dff7e7bf6be2a7ed7a12e9c2e5fc8cb7ab40e03
Rbustan0 <59246279+Rbustan0@users.noreply.github.com>
2023-05-06 22:45:36 -0700

    made comments for what to produce in controller

---
+++ controllers/thoughtController.js
@@ -0,0 +1,17 @@
+//! /api/thoughts
+
+// 1. GET all thoughts:
+
+// 2. GET a single thought by its _id
+
+// 3. POST to create a new thought (dont forget to push the created thought's _id to the associated user's thoughts array field)
+
+//  4. PUT to update a thought by its _id
+
+//  5. DELETE to remove a thought by its _id
+
+//! api/thoughts/:thoughtsId/reactions
+
+// 1. POST to create a reaction stored in a single thought's reactions array field.
+
+// 2. DELETE to pull and remove a reaction by the reaction';s reactionId value.
---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+//! (api/users/) functions
+
+// 1. GET all users:
+
+// 2. Get a single user by its _id and populated thought and friend data:
+
+// 3. POST a new user with a body:
+
+// 4. PUT a user by its _id
+
+// 5. DELETE to remove a user by its _id
+
+
+
+//! /api/users/:userId/friends/:friendId
+
+// 1. POST to add new friend to a user's friends list:
+
+// 2. DELETE to remove a friend from a user's friends list:
+
+
