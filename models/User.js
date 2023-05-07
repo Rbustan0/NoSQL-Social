da5a23ec417c41ce1b3e7e03b8534e532809976b
Rbustan0 <59246279+Rbustan0@users.noreply.github.com>
2023-05-06 21:41:23 -0700

    completed User model

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, Model } = require('mongoose');
+// require this allows me to make some code below shorter.
+const Thought = require('./Thought');
+
+// Schema for User:
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // Found on site: https://masteringjs.io/tutorials/mongoose/mongoose-validate-unique-email Validates min one character before the @, before the . for the site, and then one after the . for the site
+            match: [/.+\@.+\..+/, 'Please enter a valid email address']
+        },
+        thoughts: [Thought],
+        // Self references (dont want to take chance)
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+
+    }
+);
+
+// Virtual property to get the length of the user's friends array.
+
+userSchema
+
+.virtual('friendCount')
+
+.get(function () {return this.friends.length});
+
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
