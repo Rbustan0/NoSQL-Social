b6514382f7af73975805647771e7c90c08775c14
Rbustan0 <59246279+Rbustan0@users.noreply.github.com>
2023-05-08 09:39:44 -0700

    added some seed info

--- utils/seed.js
+++ utils/seed.js
@@ -1,1 +1,27 @@
 // !WORK HERE SECOND THING (then proceeed to controller)
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+
+// const { getRandomUserEmail, getRandomThoughts } = require('/.data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+    console.log('connected to server');
+    await Thought.deleteMany({});
+    await User.deleteMany({});
+    
+    const users = [];
+    //const thoughts = getRandomThoughts(?);
+
+    // for loops gp here
+
+
+    await User.collection.insertMany(users);
+    await Thought.collection.insertMany(thoughts);
+});
+
+console.table(users);
+console.table(thoughts);
+console.info('Seeding complete! 🌱');
+process.exit(0);
