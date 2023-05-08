645dee1a99a762c610e010e91e2ecdbd17ee87c4
Rbustan0 <59246279+Rbustan0@users.noreply.github.com>
2023-05-08 00:44:28 -0700

    added seeds for User Schema

--- utils/data.js
+++ utils/data.js
@@ -1,1 +1,54 @@
 // !WORK HERE FIRST THING!
+
+
+// Data that follows User Schema
+
+
+const username = [
+"user123",
+"randomemail456",
+"testemail789",
+"emailuser321",
+"myemailaddress",
+"usernameemail",
+"email1234",
+"emailuser567",
+"exampleemail789",
+"randomuseremail",
+"emailtest123",
+"useremail456",
+"email12345",
+"emailuser678",
+"example789email",
+"emailaddress123",
+"useremail567",
+"emailuserexample",
+"emailtest789",
+"randomemailuser"
+];
+
+
+
+
+const emailSamples = [
+'user123@example.com',
+'randomemail456@example.com',
+'testemail789@example.com',
+'emailuser321@example.com',
+'myemailaddress@example.com',
+'usernameemail@example.com',
+'email1234@example.com',
+'emailuser567@example.com',
+'exampleemail789@example.com',
+'randomuseremail@example.com',
+'emailtest123@example.com',
+'useremail456@example.com',
+'email12345@example.com',
+'emailuser678@example.com',
+'example789email@example.com',
+'emailaddress123@example.com',
+'useremail567@example.com',
+'emailuserexample@example.com',
+'emailtest789@example.com',
+'randomemailuser@example.com'
+];
