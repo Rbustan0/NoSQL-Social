b507c2bcb59607cc2edd10fd21429591fbb7341c
Rbustan0 <59246279+Rbustan0@users.noreply.github.com>
2023-05-08 19:20:25 -0700

    working on UserController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,65 @@
+const { User, Thought } = require('../models');
+
 //! (api/users/) functions
+//Going to write everything inside module exports to save time.
+// Also prefer async functions to .then() and .catch() functions
 
-// 1. GET all users:
+module.exports = {
+    
+    // 1. GET all users:
+    getAllUsers: async (req, res) => {
+        try{
+            const users = await User.find();
+            res.status(200).json(users);
+        }
+        catch(error){
+            console.error(error);
+            res.status(500).json( { error: 'Failed to get all users'});
+        }
 
-// 2. Get a single user by its _id and populated thought and friend data:
+    },    
+        
+    
+    // 2. Get a single user by its _id and populated thought and friend data:
+    getSingleUser: async (req, res) => {
+       try{
+        const user = await User.findOne({_id: req.params.userId})
+       .select('-__v') //excluded from get
+       .populate('thoughts friends'); //includes sub/embedded docs in user schema
+        
+       !user 
+       ? res.status(404).json( { error: ' User not found' })
+       : res.status(200).json(user);
+       }
 
-// 3. POST a new user with a body:
+       catch(error) {
+        console.error(error);
+        res.status(500).json( { error: 'Server Issue' } )
+       }
+    },
+    
+    // 3. POST a new user with a body:
+    
+    createUser: async (req, res) => {
+        try{
+            const user = await User.create(req.body);
+            res.status(201).json(user);
+        }
+        catch(error) {
+            console.error(error);
+            res.status(500).json( { error: 'Server Issue' });
+        }
+    },
 
-// 4. PUT a user by its _id
+    // 4. PUT a user by its _id
+    
+    updateUser: async (req, res) =>
+    // 5. DELETE to remove a user by its _id
 
-// 5. DELETE to remove a user by its _id
+
+
+};
+
 
 
 
