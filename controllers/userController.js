b56c1e4c2809ce562da3b0292dac959dd8838bb1
Rbustan0 <59246279+Rbustan0@users.noreply.github.com>
2023-05-08 19:39:40 -0700

    finished api/user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -53,10 +53,41 @@ module.exports = {
 
     // 4. PUT a user by its _id
     
-    updateUser: async (req, res) =>
+    updateUser: async (req, res) => {
+        try{
+            const updatedUser = await User.findByIdAndUpdate(
+                req.params.userId,
+                req.body,
+                { new: true }
+            );
+
+            !updatedUser
+            ? res.status(404).json({ error: 'User not found' })
+            : res.status(200).json(updatedUser);
+        }
+        catch(error) {
+            console.error(error);
+            res.status(500).json({ error: 'Server Issue' });
+        }
+    },
+
     // 5. DELETE to remove a user by its _id
 
-
+    deleteUser: async (req,res) => {
+        try {
+            const deletedUser = await User.findByIdAndDelete(req.params.userId)
+            !deletedUser
+            ? res.status(404).json({ error: 'User not found'})
+            : res.status(200).json({ message: 'User was deleted!'});
+            
+            // BONUS Delete associated thoughts
+            await Thought.deleteMany({ username: deletedUser.username });
+        }
+        catch(error) {
+            console.error(error);
+            res.status(500).json({ error: 'Server Issue'});
+        }
+    }
 
 };
 
