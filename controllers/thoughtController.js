c8261ffbcd1751c5013342779769f668ca2db2d3
Rbustan0 <59246279+Rbustan0@users.noreply.github.com>
2023-05-08 20:35:02 -0700

    finished /api/thoughts controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,17 +1,97 @@
-//! /api/thoughts
+const { User, Thought } = require('../models');
 
-// 1. GET all thoughts:
+module.exports = {
 
-// 2. GET a single thought by its _id
+    //! /api/thoughts
 
-// 3. POST to create a new thought (dont forget to push the created thought's _id to the associated user's thoughts array field)
+    // 1. GET all thoughts:
+    getAllThoughts: async (req, res) => {
+        try {
+            const thoughts = await Thought.find();
+            res.status(200).json(thoughts);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Failed to get all thoughts' });
+        }
+    },
+    // 2. GET a single thought by its _id
+    getSingleThought: async (req, res) => {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId });
+            !thought
+                ? res.status(404).json({ error: 'Thought not found' })
+                : res.status(200).json(thought);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Server Issue' });
+        }
+    },
+    // 3. POST to create a new thought (dont forget to push the created thought's _id to the associated user's thoughts array field)
 
-//  4. PUT to update a thought by its _id
+    createThought: async (req, res) => {
+        try {
+            const thought = await Thought.create(req.body);
+            const user = await User.findByIdAndUpdate(
+                req.body.userId,
+                { $push: { thoughts: thought._id } }, //adds element to array field
+                { new: true }
+            );
+            !user
+                ? res.status(404).json({ error: 'User not found' })
+                : res.status(201).json(thought);
 
-//  5. DELETE to remove a thought by its _id
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Server Issue' });
+        }
+    },
 
-//! api/thoughts/:thoughtsId/reactions
+    //  4. PUT to update a thought by its _id
+    updateThought: async (req, res) => {
+        try {
+            const updatedThought = await Thought.findByIdAndUpdate(
+                req.params.thoughtId,
+                req.body,
+                { new: true }
+            );
 
-// 1. POST to create a reaction stored in a single thought's reactions array field.
+            !updatedThought
+                ? res.status(404).json({ error: 'Thought not found' })
+                : res.status(200).json(updatedThought);
 
-// 2. DELETE to pull and remove a reaction by the reaction';s reactionId value.
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Server Issue' });
+        }
+    },
+
+    //  5. DELETE to remove a thought by its _id
+    deleteThought: async (req, res) => {
+        try {
+            const deletedThought = await Thought.findByIdAndDelete(req.params.thoughtId);
+
+            if (!deletedThought) {
+                return res.status(404).json({ error: 'Thought not found' });
+            }
+
+            // Remove the thought from the associated user's thoughts array
+            // NEED THIS due to no cascade defined in model
+            await User.findByIdAndUpdate(
+                deletedThought.userId,
+                { $pull: { thoughts: deletedThought._id } }
+            );
+
+            res.status(200).json({ message: 'Thought was deleted!' });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Server Issue' });
+        }
+    }
+
+    //! api/thoughts/:thoughtsId/reactions
+
+    // 1. POST to create a reaction stored in a single thought's reactions array field.
+
+    // 2. DELETE to pull and remove a reaction by the reaction';s reactionId value.
+
+}
--- controllers/userController.js
+++ controllers/userController.js
@@ -5,56 +5,56 @@ const { User, Thought } = require('../models');
 // Also prefer async functions to .then() and .catch() functions
 
 module.exports = {
-    
+
     // 1. GET all users:
     getAllUsers: async (req, res) => {
-        try{
+        try {
             const users = await User.find();
             res.status(200).json(users);
         }
-        catch(error){
+        catch (error) {
             console.error(error);
-            res.status(500).json( { error: 'Failed to get all users'});
+            res.status(500).json({ error: 'Failed to get all users' });
         }
 
-    },    
-        
-    
+    },
+
+
     // 2. Get a single user by its _id and populated thought and friend data:
     getSingleUser: async (req, res) => {
-       try{
-        const user = await User.findOne({_id: req.params.userId})
-       .select('-__v') //excluded from get
-       .populate('thoughts friends'); //includes sub/embedded docs in user schema
-        
-       !user 
-       ? res.status(404).json( { error: ' User not found' })
-       : res.status(200).json(user);
-       }
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+                .select('-__v') //excluded from get
+                .populate('thoughts friends'); //includes sub/embedded docs in user schema
 
-       catch(error) {
-        console.error(error);
-        res.status(500).json( { error: 'Server Issue' } )
-       }
+            !user
+                ? res.status(404).json({ error: ' User not found' })
+                : res.status(200).json(user);
+        }
+
+        catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Server Issue' })
+        }
     },
-    
+
     // 3. POST a new user with a body:
-    
+
     createUser: async (req, res) => {
-        try{
+        try {
             const user = await User.create(req.body);
             res.status(201).json(user);
         }
-        catch(error) {
+        catch (error) {
             console.error(error);
-            res.status(500).json( { error: 'Server Issue' });
+            res.status(500).json({ error: 'Server Issue' });
         }
     },
 
     // 4. PUT a user by its _id
-    
+
     updateUser: async (req, res) => {
-        try{
+        try {
             const updatedUser = await User.findByIdAndUpdate(
                 req.params.userId,
                 req.body,
@@ -62,10 +62,10 @@ module.exports = {
             );
 
             !updatedUser
-            ? res.status(404).json({ error: 'User not found' })
-            : res.status(200).json(updatedUser);
+                ? res.status(404).json({ error: 'User not found' })
+                : res.status(200).json(updatedUser);
         }
-        catch(error) {
+        catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Server Issue' });
         }
@@ -73,34 +73,34 @@ module.exports = {
 
     // 5. DELETE to remove a user by its _id
 
-    deleteUser: async (req,res) => {
+    deleteUser: async (req, res) => {
         try {
             const deletedUser = await User.findByIdAndDelete(req.params.userId)
             !deletedUser
-            ? res.status(404).json({ error: 'User not found'})
-            : res.status(200).json({ message: 'User was deleted!'});
-            
+                ? res.status(404).json({ error: 'User not found' })
+                : res.status(200).json({ message: 'User was deleted!' });
+
             // BONUS: Delete associated thoughts
             await Thought.deleteMany({ username: deletedUser.username });
         }
-        catch(error) {
+        catch (error) {
             console.error(error);
-            res.status(500).json({ error: 'Server Issue'});
+            res.status(500).json({ error: 'Server Issue' });
         }
     },
 
     //! /api/users/:userId/friends/:friendId
-    
+
     // 1. POST to add new friend to a user's friends list:
 
-    addFriend: async(req,res) => {
-        try{
+    addFriend: async (req, res) => {
+        try {
             // destruct for use in function
             const { userId, friendId } = req.params;
 
             const user = await User.findByIdAndUpdate(
                 userId,
-                { $addToSet: { friends: friendId }}, //prevents duplicate adds
+                { $addToSet: { friends: friendId } }, //prevents duplicate adds
                 { new: true }
             );
 
@@ -108,16 +108,16 @@ module.exports = {
                 ? res.status(404).json({ error: 'User not found' })
                 : res.status(200).json(user);
         }
-        catch(error){
+        catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Server Issue' });
         }
     },
-    
+
     // 2. DELETE to remove a friend from a user's friends list:
 
     removeFriend: async (req, res) => {
-        try{
+        try {
             const { userId, friendId } = req.params;
 
             const user = await User.findByIdAndUpdate(
@@ -130,7 +130,7 @@ module.exports = {
                 ? res.status(404).json({ error: 'User not found' })
                 : res.status(200).json(user);
         }
-        catch(error){
+        catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Server Issue' });
         }
